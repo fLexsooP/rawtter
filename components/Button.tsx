d641e3eb932d2184e7193412f8d4b30a21fe6463
Chence Shi <ascc594557067@gmail.com>
2023-05-24 23:46:29 -0700

    fix: fix submit button

--- components/Button.tsx
+++ components/Button.tsx
@@ -21,6 +21,8 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
+      disabled={disabled}
+      onClick={onClick}
       className={`
         disabled:opacity-70
         disabled:cursor-not-allowed

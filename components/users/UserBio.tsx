98d722006477e372a8b1975aa000a9c8c12169f1
Chence Shi <ascc594557067@gmail.com>
2023-05-26 16:47:32 -0700

    add user profile page

---
+++ components/users/UserBio.tsx
@@ -0,0 +1,44 @@
+import { use, useMemo } from 'react';
+import { format } from 'date-fns';
+
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useUser from '@/hooks/useUser';
+
+import Button from '../Button';
+
+interface UserBioProps {
+  userId: string;
+}
+
+const UserBio: React.FC<UserBioProps> = ({ userId }) => {
+  const { data: currentUser } = useCurrentUser();
+  const { data: fetchedUser } = useUser(userId);
+
+  const createdAt = useMemo(() => {
+    if (!fetchedUser?.createdAt) return null;
+
+    return format(new Date(fetchedUser.createdAt), 'MMMM yyyy');
+  }, [fetchedUser?.createdAt]);
+
+  return (
+    <div className="border-b-[1px] border-neutral-800 pb-4">
+      <div className="flex justify-end p-2">
+        {currentUser?.id === userId ? (
+          <Button
+            label="Edit"
+            onClick={() => {}}
+            secondary
+          />
+        ) : (
+          <Button
+            label="Follow"
+            onClick={() => {}}
+            secondary
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default UserBio;

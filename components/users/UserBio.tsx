a40d38ae91ef18a5f5477a7fef733c9b7c7d4269
Chence Shi <ascc594557067@gmail.com>
2023-05-30 01:18:05 -0700

    feat: add user following

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -5,6 +5,7 @@ import { BiCalendar } from 'react-icons/bi';
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useUser from '@/hooks/useUser';
 import useEditModal from '@/hooks/useEditModal';
+import useFollow from '@/hooks/useFollow';
 
 import Button from '../Button';
 
@@ -17,6 +18,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: fetchedUser } = useUser(userId);
 
   const editModal = useEditModal();
+
+  const {isFollowing, toggleFollow} = useFollow(userId);
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) return null;
@@ -35,9 +38,10 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           />
         ) : (
           <Button
-            label="Follow"
-            onClick={() => {}}
-            secondary
+            label={isFollowing ? 'Unfollow' : 'Follow'}
+            onClick={toggleFollow}
+            secondary={!isFollowing}
+            outline={isFollowing}
           />
         )}
       </div>
---
+++ hooks/useFollow.ts
@@ -0,0 +1,53 @@
+import { useCallback, useMemo } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
+import useCurrentUser from './useCurrentUser';
+import useUser from './useUser';
+import useLoginModal from './useLoginModal';
+
+const useFollow = (userId: string) => {
+  const { data: currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(userId);
+
+  const loginModal = useLoginModal();
+
+  const isFollowing = useMemo(() => {
+    const followingList = currentUser?.followingIds || [];
+    return followingList.includes(userId);
+  }, [userId, currentUser]);
+
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      loginModal.onOpen();
+    }
+
+    try {
+      let request;
+
+      if (isFollowing) {
+        request = () => axios.delete('/api/follow', { params: { userId } });
+      } else {
+        request = () => axios.post('/api/follow', { userId });
+      }
+      await request();
+
+      mutateCurrentUser();
+      mutateFetchedUser();
+      toast.success('Success');
+    } catch (error) {
+      toast.error('Failed to follow');
+    }
+  }, [
+    currentUser,
+    isFollowing,
+    userId,
+    mutateCurrentUser,
+    mutateFetchedUser,
+    loginModal,
+  ]);
+
+  return { isFollowing, toggleFollow };
+};
+
+export default useFollow;
